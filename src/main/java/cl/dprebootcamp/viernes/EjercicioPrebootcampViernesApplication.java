0ec66e505de3953712c38da4a0ffec5cb87249fa
Juan Pablo Duran <pablo_duran@unicomer.com>
2024-01-26 18:20:24 -0300

    primera carga

---
+++ src/main/java/cl/dprebootcamp/viernes/EjercicioPrebootcampViernesApplication.java
@@ -0,0 +1,13 @@
+package cl.dprebootcamp.viernes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioPrebootcampViernesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioPrebootcampViernesApplication.class, args);
+	}
+
+}
